645289bc5b6a2b726383779e905870ba2956c100
Artem Shvets <150855250+MargoshaGit@users.noreply.github.com>
2024-02-29 19:30:59 +0200

    ASP exam

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿bool IsPalindrome(int number)
+{
+    string numberStr = number.ToString();
+    int length = numberStr.Length;
+
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (numberStr[i] != numberStr[length - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Console.WriteLine(IsPalindrome(838));
+Console.WriteLine(IsPalindrome(511115));
+Console.WriteLine(IsPalindrome(443344));
